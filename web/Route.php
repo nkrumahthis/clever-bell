ea413fae29891028bcbcd8aea25767c4f94d47a7
nkrumahinc <nkrumahthis@gmail.com>
2021-04-08 16:59:21 +0000

    started route.php

---
+++ web/Route.php
@@ -0,0 +1,57 @@
+<?php
+
+class Route
+{
+    private static $routes = array();
+    private static $pathNotFound = null;
+    private static $methodNotAllowed = null;
+
+    public static function add($expression, $function, $method = 'get')
+    {
+        array_push(self::$routes, array(
+            'expression' => $expression,
+            'function' => $function,
+            'method' => $method
+        ));
+    }
+
+    public static function run($basepath = '/')
+    {
+        // Parse current url
+        $parsed_url = parse_url($_SERVER['REQUEST_URI']); //Parse Uri
+
+        if (isset($parsed_url['path'])) {
+            $path = $parsed_url['path'];
+        } else {
+            $path = '/';
+        }
+
+        // Get current request method
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        $path_match_found = false;
+
+        $route_match_found = false;
+
+        foreach (self::$routes as $route) {
+
+            // If the method matches check the path
+
+            // Add basepath to matching string
+            if ($basepath != '' && $basepath != '/') {
+                $route['expression'] = '(' . $basepath . ')' . $route['expression'];
+            }
+
+            // Add 'find string start' automatically
+            $route['expression'] = '^' . $route['expression'];
+
+            // Add 'find string end' automatically
+            $route['expression'] = $route['expression'] . '$';
+        }
+    }
+
+    public static function pathNotFound($function)
+    {
+        self::$pathNotFound = $function;
+    }
+}
