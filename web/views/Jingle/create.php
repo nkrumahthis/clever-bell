de37d62c46ab357d9c26f4089caabf0912c40a42
nkrumahinc <nkrumahthis@gmail.com>
2021-04-20 03:40:44 +0000

    dev: jingles upload

---
+++ web/views/Jingle/create.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/styles.css">
+    <script src="/js/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
+    <title>CleverBell Editor : Create Schedule</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mt-5">Upload Jingle</h2>
+                    <p>Please choose a file to upload</p>
+                    <form action="jingle" enctype="multipart/form-data" method="post">
+                        <input type="file" name="jingle" accept="audio/*">
+                        <input type="submit" name="Submit" value="Submit">
+                    </form>
+
+                    <button>Upload Sound</button>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
